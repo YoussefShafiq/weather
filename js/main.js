098bc783e73a82cdd9b97508073d8f7caeaa7740
youssef shafek <youssefmsa616@gmail.com>
2024-06-14 21:44:57 +0300

    1. add automatic location 2. style update

--- js/main.js
+++ js/main.js
@@ -10,8 +10,14 @@ searchInput.addEventListener('input', function () {
     getWeather(getcountry)
 })
 
-let getcountry = 'cairo'
+async function getcountry() {
+    let data = await fetch(`https://ipinfo.io/json`)
+    let result = await data.json()
+    console.log(result.city);
+    getWeather(result.city)
+}
 
+getcountry()
 async function getWeather(country) {
     let data = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=1f039acad4d9499b854172033241206&q=${country}&days=3&aqi=no&alerts=no`)
     let result = await data.json()
@@ -21,9 +27,9 @@ async function getWeather(country) {
 
 }
 
-getWeather(getcountry)
 
 function displayToday(newcountry, newDeg, newCondition, newHumadity, newWind, newDiraction, date) {
+    weatherBackground(`${newCondition.text}`, 'todaycard')
     document.getElementById('todayName').innerHTML = getDayName(`${date}`)
     document.getElementById('Date').innerHTML = formatDate(`${date}`)
     country.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${newcountry} `
@@ -35,18 +41,20 @@ function displayToday(newcountry, newDeg, newCondition, newHumadity, newWind, ne
 }
 
 function displayDay2(maxDeg, minDeg, condition, date) {
+    weatherBackground(`${condition.text}`, 'day2card')
     document.getElementById('day2Name').innerHTML = getDayName(`${date}`)
     document.getElementById('day2conditionIcon').innerHTML = `<img src="${condition.icon}" class="w-25" alt="">`
-    document.getElementById('day2maxC').innerHTML = `${maxDeg} ْ C`
-    document.getElementById('day2minC').innerHTML = `${minDeg} ْ C`
+    document.getElementById('day2maxC').innerHTML = `${maxDeg} ْC`
+    document.getElementById('day2minC').innerHTML = `${minDeg} ْC`
     document.getElementById('day2condition').innerHTML = `${condition.text}`
 }
 
 function displayDay3(maxDeg, minDeg, condition, date) {
+    weatherBackground(`${condition.text}`, 'day3card')
     document.getElementById('day3Name').innerHTML = getDayName(`${date}`)
     document.getElementById('day3conditionIcon').innerHTML = `<img src="${condition.icon}" class="w-25" alt="">`
-    document.getElementById('day3maxC').innerHTML = `${maxDeg} ْ C`
-    document.getElementById('day3minC').innerHTML = `${minDeg} ْ C`
+    document.getElementById('day3maxC').innerHTML = `${maxDeg} ْC`
+    document.getElementById('day3minC').innerHTML = `${minDeg} ْC`
     document.getElementById('day3condition').innerHTML = `${condition.text}`
 }
 
@@ -65,6 +73,66 @@ function formatDate(dateString) {
 
     return `${day}${month}`;
 }
+
+function weatherBackground(condition, cardid) {
+    document.getElementById(cardid).classList.remove('Sunnybg', 'cloudybg', 'Clearbg', 'partlycloudybg', 'rainbg', 'patchyrainbg', 'mistbg', 'thunderbg', 'freezerainbg')
+    switch (condition) {
+        case 'Sunny':
+            document.getElementById(cardid).classList.add('Sunnybg')
+            break;
+        case 'Cloudy':
+            document.getElementById(cardid).classList.add('cloudybg')
+            break;
+        case 'Overcast ':
+            document.getElementById(cardid).classList.add('cloudybg')
+            break;
+        case 'Overcast':
+            document.getElementById(cardid).classList.add('cloudybg')
+            break;
+        case 'Clear':
+            document.getElementById(cardid).classList.add('Clearbg')
+            break;
+        case 'Partly cloudy':
+            document.getElementById(cardid).classList.add('partlycloudybg')
+            break;
+        case 'Partly Cloudy':
+            document.getElementById(cardid).classList.add('partlycloudybg')
+            break;
+        case 'Partly Cloudy ':
+            document.getElementById(cardid).classList.add('partlycloudybg')
+            break;
+        case 'Patchy rain nearby':
+            document.getElementById(cardid).classList.add('patchyrainbg')
+            break;
+        case 'Moderate rain':
+            document.getElementById(cardid).classList.add('rainbg')
+            break;
+        case 'Light rain':
+            document.getElementById(cardid).classList.add('rainbg')
+            break;
+        case 'Light rain':
+            document.getElementById(cardid).classList.add('mistbg')
+            break;
+        case 'mist':
+            document.getElementById(cardid).classList.add('mistbg')
+            break;
+        case 'Moderate or heavy rain with thunder':
+            document.getElementById(cardid).classList.add('thunderbg')
+            break;
+        case 'Patchy light rain with thunder':
+            document.getElementById(cardid).classList.add('thunderbg')
+            break;
+        case 'Heavy rain':
+            document.getElementById(cardid).classList.add('thunderbg')
+            break;
+        case 'Light freezing rain':
+            document.getElementById(cardid).classList.add('freezerainbg')
+            break;
+
+
+    }
+}
+
 
 
 
